5b9e2d757fb13b44c15273581f12fac3ce47c67f
Ariel <arielcedillo225@gmail.com>
2025-05-14 19:12:54 -0500

    Feat: Create Repository, Service and Controller for creating Invoices

---
+++ src/main/java/com/cat/msa/invoices/service/impl/InvoiceHeaderServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cat.msa.invoices.service.impl;
+
+import com.cat.msa.invoices.domain.InvoiceHeader;
+import com.cat.msa.invoices.repository.InvoiceHeaderRepository;
+import com.cat.msa.invoices.service.InvoiceHeaderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
+    private final InvoiceHeaderRepository invoiceHeaderRepository;
+
+    public InvoiceHeaderServiceImpl(InvoiceHeaderRepository invoiceHeaderRepository) {
+        this.invoiceHeaderRepository = invoiceHeaderRepository;
+    }
+
+
+    @Override
+    public InvoiceHeader createInvoiceHeader(InvoiceHeader invoiceHeader) {
+        invoiceHeader.calculateInvoiceAmount();
+        return invoiceHeaderRepository.save(invoiceHeader);
+    }
+}
